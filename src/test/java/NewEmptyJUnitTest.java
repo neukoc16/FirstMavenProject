0824fab5e16e1a7bfdb144caf4752ff3c4eebfdc
neukoc16 <ko.neubauer@gmail.com>
2019-03-21 08:55:02 +0100

    deactivated tests

--- src/test/java/NewEmptyJUnitTest.java
+++ src/test/java/NewEmptyJUnitTest.java
@@ -28,21 +28,21 @@ public class NewEmptyJUnitTest {
     public void tearDown() {
     }
 
-    @Test
-    public void testJahresAfa() {
-        Anlage a = new Anlage(2000.0, 10);
-        assertEquals(200.0, a.jahresAfa(), 0.01);
-    }
-
-    public void testBuchwert1() {
-        Anlage a = new Anlage(2000.0, 10);
-        a.updateBuchwert(5);
-        assertEquals(100.0, a.getBuchwert(), 0.01);
-    }
-
-    public void testBuchwert2() {
-        Anlage a = new Anlage(2000.0, 10);
-        a.updateBuchwert(12);
-        assertEquals(1.0, a.getBuchwert(), 0.01);
-    }
+//    @Test
+//    public void testJahresAfa() {
+//        Anlage a = new Anlage(2000.0, 10);
+//        assertEquals(200.0, a.jahresAfa(), 0.01);
+//    }
+//
+//    public void testBuchwert1() {
+//        Anlage a = new Anlage(2000.0, 10);
+//        a.updateBuchwert(5);
+//        assertEquals(100.0, a.getBuchwert(), 0.01);
+//    }
+//
+//    public void testBuchwert2() {
+//        Anlage a = new Anlage(2000.0, 10);
+//        a.updateBuchwert(12);
+//        assertEquals(1.0, a.getBuchwert(), 0.01);
+//    }
 }
